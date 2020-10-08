22b4ef5a6c870889f2c0444bfe221d7d1da6909b
DFastje <dfastje@gmail.com>
2020-10-07 20:02:57 -0400

    Initial commit after generating project

---
+++ src/main/java/me/dfastje/ScrabbleProject/ScrabbleProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dfastje.ScrabbleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrabbleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrabbleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dfastje/ScrabbleProject/ScrabbleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dfastje.ScrabbleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
